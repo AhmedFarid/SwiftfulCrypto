2bcf750f78bac8429025336e09357b8a7823d9a4
ahmed farid <engfaridooo@gmail.com>
2024-09-10 22:38:17 +0300

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Farido on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
